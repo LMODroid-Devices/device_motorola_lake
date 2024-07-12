555ed7f2e081e33b2e3e4e65a88e8ae54026fc0e
LMO GerritBot <gerrit@libremobileos.com>
2024-07-12 06:30:08 +0000

    Adapt to LMODroid by device-add script

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lake.mk
+    $(LOCAL_DIR)/lmodroid_lake.mk
--- lmodroid_lake.mk
+++ lmodroid_lake.mk
@@ -1,6 +1,6 @@
 # Inherit some common Lineage stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Device
@@ -12,7 +12,7 @@ PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := lake
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_MODEL := moto g(7) plus
-PRODUCT_NAME := lineage_lake
+PRODUCT_NAME := lmodroid_lake
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=lake \
